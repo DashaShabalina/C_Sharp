d38628d83684bba438bc6cf8312f6e19e664d025
DashaShabalina <67578446+DashaShabalina@users.noreply.github.com>
2022-01-17 17:13:40 +0700

    feat(tests): BehaviourServiceTest fixed

--- Tests/WorldOfWorms.Tests/BehaviourServiceTest.cs
+++ Tests/WorldOfWorms.Tests/BehaviourServiceTest.cs
@@ -1,12 +1,59 @@
-﻿using System;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WorldofWorms.Data;
 
 namespace WorldOfWorms.Tests
 {
-    internal class BehaviourServiceTest
+    public class BehaviourServiceTest
     {
+        private DbContextOptions<EnvironmentContext> options;
+
+        [SetUp]
+        public void SetUpDb()
+        {
+            options = new DbContextOptionsBuilder<EnvironmentContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+        }
+
+        [Test]
+        public void ShouldBeRightWorkedBehaviourService()
+        {
+            using var context = new EnvironmentContext(options);
+            var foodGenerator = new FoodGenerator();
+
+            var service = new BehaviourService(context, foodGenerator);
+
+            var behaviourName = "Test";
+            service.GenerateBehavior(context, behaviourName, foodGenerator);
+            service.steps.Should().NotBeNull();
+            service.steps.Should().NotBeEmpty();
+            service.behavior.Name.Should().Be(behaviourName);
+
+            var listbehaviour = new List<Сoordinates>();
+            var state = new World();
+            var name = context.Behaviours.Where(b => b.Name == behaviourName);
+
+            IEnumerable<BehaviorInfo> behaviors = service.GetBehavior(context, behaviourName);
+
+            foreach (var j in behaviors)
+            {
+                listbehaviour.Add(new Сoordinates(j.X, j.Y));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Food curFood = new Food(listbehaviour[i].X, listbehaviour[i].Y);
+
+                FoodController foodController = new FoodController(curFood);
+                foodController.ControlFood(state);
+
+                state.Food[i].X.Should().Be(curFood.X);
+                state.Food[i].Y.Should().Be(curFood.Y);
+            }
+        }
     }
 }
